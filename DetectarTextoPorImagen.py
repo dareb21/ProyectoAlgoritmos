4b272fde6802737a0ae19eba9bcb050eb187d0af
dareb21 <162059543+dareb21@users.noreply.github.com>
2024-11-04 20:53:20 -0600

    Proto interfaz

--- DetectarTextoPorImagen.py
+++ DetectarTextoPorImagen.py
@@ -1,9 +1,36 @@
+
+#Bibliotecas necesarias: Pillow, pytesseract, pathlib, tkinter
+
+from importlib.metadata import files
+from tkinter.filedialog import askopenfilename
 from PIL import Image
 from pytesseract import *
-
+from pathlib import Path
+import tkinter as tk
+import os
 # Aca colocarán la dirección de su máquina, solo que comentenla para no interferir con la máquina de los demás
-# pytesseract.tesseract_cmd = r"C:\Users\Dareb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+pytesseract.tesseract_cmd = r"C:\Users\Dareb\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
-img = Image.open("img1.JPG")
-text = pytesseract.image_to_string(img, lang='spa')
-print(text)
+def DetectarTextoPorImagen():
+    file=askopenfilename()
+    file_name=os.path.basename(file)
+    print(file_name)
+    text = pytesseract.image_to_string(Image.open(file_name))
+    lbl_texto.config(text=text)
+    #print(text)
+
+
+root = tk.Tk()
+
+root.geometry('800x500')
+root.title("Detectar Texto de Imagen")
+btn_convertir = tk.Button(root, text="Seleccione imagen", command=DetectarTextoPorImagen)
+btn_convertir.place(x=50, y=150)
+
+lbl_titulo = tk.Label(root, text="Su texto aparecera aqui: ")
+lbl_titulo.place(x=50, y=200)
+lbl_texto = tk.Label()
+lbl_texto.place(x=50, y=300)
+
+root.mainloop()
+
